9967d49ba52003f05472435db44c2270d720ca48
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-01-06 20:38:40 -0500

    Creates state management project

---
+++ SwiftUI/StateManagement/StateManagement/StateManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateManagementApp.swift
+//  StateManagement
+//
+//  Created by Alonso on 6/01/24.
+//
+
+import SwiftUI
+
+@main
+struct StateManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
