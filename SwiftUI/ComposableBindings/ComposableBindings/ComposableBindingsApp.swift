0bab76e5f96ed46f955f5bdb2ba2d9587fbe6169
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-01-07 21:12:59 -0500

    Composable Bindings: Create project

---
+++ SwiftUI/ComposableBindings/ComposableBindings/ComposableBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableBindingsApp.swift
+//  ComposableBindings
+//
+//  Created by Alonso on 7/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ComposableBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
