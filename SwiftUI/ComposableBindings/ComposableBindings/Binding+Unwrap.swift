c363b437b10d93cf9f77b4bd8b831f36556aee0b
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-01-21 17:27:40 -0500

    Composable Bindings: Bindings of enums

--- SwiftUI/ComposableBindings/ComposableBindings/Binding+Unwrap.swift
+++ SwiftUI/ComposableBindings/ComposableBindings/Binding+Unwrap.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 extension Binding {
-
     func unwrap<Wrapped>() -> Binding<Wrapped>? where Value == Wrapped? {
         guard let value = wrappedValue else { return nil }
         return Binding<Wrapped>(get: { value }, set: { self.wrappedValue = $0 })
     }
 
+    func matching<Case>(extract: @escaping (Value) -> Case?, embed: @escaping (Case) -> Value) -> Binding<Case>? {
+        guard let `case` = extract(wrappedValue) else { return nil }
+        return Binding<Case>(get: { `case` }, set: { `case` in self.wrappedValue = embed(`case`) })
+    }
 }
