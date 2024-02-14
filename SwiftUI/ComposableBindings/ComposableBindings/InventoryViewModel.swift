680963ec7ded0e954cb2907de692b76eb5a4f103
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-02-14 00:20:08 -0500

    Composable Bindings: Domain modeling: adding vs. duplicating

--- SwiftUI/ComposableBindings/ComposableBindings/InventoryViewModel.swift
+++ SwiftUI/ComposableBindings/ComposableBindings/InventoryViewModel.swift
@@ -6,10 +6,12 @@
 //
 
 import Combine
+import Foundation
 
 class InventoryViewModel: ObservableObject {
     @Published var inventory: [Item]
     @Published var draft: Item?
+    // @Published var draft: Draft?
 
     init(inventory: [Item]) {
         self.inventory = inventory
@@ -17,6 +19,7 @@ class InventoryViewModel: ObservableObject {
 
     func addButtonTapped() {
         draft = Item(name: "", color: nil, status: .inStock(quantity: 1))
+        // draft = .adding(Item(name: "", color: nil, status: .inStock(quantity: 1)))
     }
 
     func cancelButtonTapped() {
@@ -28,9 +31,32 @@ class InventoryViewModel: ObservableObject {
             self.inventory.append(item)
         }
         self.draft = nil
+
+//        switch self.draft {
+//        case let .some(.duplicating(item)),
+//            let .some(.adding(item)):
+//            self.inventory.append(item)
+//
+//        case .none:
+//            break
+//        }
+//        self.draft = nil
     }
 
     func duplicate(item: Item) {
         self.draft = Item(name: item.name, color: item.color, status: item.status)
+        // self.draft = .duplicating(Item(name: item.name, color: item.color, status: item.status))
+    }
+}
+
+enum Draft: Identifiable, Hashable {
+    case adding(Item)
+    case duplicating(Item)
+
+    var id: UUID {
+        switch self {
+        case let .adding(item): return item.id
+        case let .duplicating(item): return item.id
+        }
     }
 }
